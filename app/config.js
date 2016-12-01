c9a810565e7969b18c7ed11aa889cbb9a856e325
Mark Barlow <markbarlow@users.noreply.github.com>
2016-12-01 15:58:54 +0000

    start with clean prototype, latest download

--- app/config.js
+++ app/config.js
@@ -3,9 +3,8 @@
 // Note: prototype config can be overridden using environment variables (eg on heroku)
 
 module.exports = {
-
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "MB prototype test",
+  serviceName: 'Service name goes here',
 
   // Default port that prototype runs on
   port: '3000',
@@ -13,8 +12,13 @@ module.exports = {
   // Enable or disable password protection on production
   useAuth: 'true',
 
+  // Enable or disable built-in docs and examples.
+  useDocumentation: 'true',
+
+  // Force HTTP to redirect to HTTPs on production
+  useHttps: 'true',
+
   // Cookie warning - update link to service's cookie page.
   cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
 
-};
-	
+}
